8d89099d00bbfb5e981c6f42d9c8f0b9aaa66e93
andy phu <andyphu7601@gmail.com>
2023-04-21 19:09:56 -0700

    carousel

---
+++ src/components/Description.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Description = () => {
+    return (
+        <div className='py-12 flex items-center w-screen justify-center text-2xl'>
+            <div className='flex flex-col ml-5 sm:flex-row sm:max-w-5xl justify-center text-base '>
+                <div className='text-lg flex grow-2 flex-col pr-14 font-bold '>
+                    <h1 className=' font-bold pb-10 text-2xl' >A DESTINATION</h1>
+                    <p className='text-gray-400 font-bold pb-6'> Experience authentic and refined Chinese barbecue at our elegant restaurant. Our expert chefs use only the freshest and finest ingredients to create bold and nuanced flavors. Indulge in the art of Chinese barbecue and discover why our restaurant is the perfect destination for an exquisite dining experience. Join us for an unforgettable evening of exceptional cuisine and elevated ambiance.</p>
+                </div>
+                <div className=' flex grow flex-col w-full text-gray-400 '>
+                    <div  className='pb-8'>
+                        <p className='pb-2'>PHONE</p>
+                        <p>(310) 819-8413</p>
+                    </div>
+                    <div className='pb-4'>
+                        <p className='pb-2'>LOCATION</p>
+                        <p> 1425 Artesia Blvd UNIT 21</p>
+                        <p>Gardena, CA 90248</p>
+                    </div>
+                    <div className='pb-4'>
+                        <p className='pb-2' >HOURS</p>
+                        <p> <span className=' font-bold text-gray-500'> Mon-Sun</span> 10AM-8:30PM</p>                            
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Description;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import topBBQNoodle from '../images/topBBQNoodle.jpeg'
+const Hero = () =>{
+    return(
+        <div className='mx-auto h-screen'>
+            <div className='bg-green-500 h-full w-full h-screen relative' >
+                {/*Overlay */}
+                <div className='absolute w-full h-full text-gray-200 bg-black/40 flex flex-col justify-center items-center h-screen'>
+                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold pb-6'>TASTE THE</h1>
+                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold'>DIFFERENCE</h1>
+                </div>
+                <img className='h-screen w-full w-screen object-cover' src={topBBQNoodle}/>
+                
+            </div>
+
+            {/*description */}
+            
+        </div>
+
+            
+        
+    )
+}
+
+export default Hero
