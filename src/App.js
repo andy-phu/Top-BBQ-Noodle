21d7b8fb6859f6b1f7340cfe7f902c7ccce0a1f1
andy phu <andyphu7601@gmail.com>
2023-04-21 19:07:13 -0700

    before carousel:
    :

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/NavBar'
+import Hero from './components/Hero'
+import Description from './components/Description'
+import PhotoGallery from './components/PhotoGallery';
+
 
 function App() {
   return (
-    <div>
+    <div className='overflow-x-hidden '>
       <Navbar />
+      <Hero />
+      <Description />
+      <PhotoGallery/>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
-import { whileStatement } from "@babel/types";
+//import { whileStatement } from "@babel/types";
 import React, {useState} from "react"
-import { AiOutlineMenu, AiOutlineClose,AiFillHome } from "react-icons/ai"
-
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
+import topBBQLogo from '../images/topBBQLogo.png'
 
 
 
@@ -9,20 +9,19 @@ const Navbar = () => {
     const [nav,setNav] = useState(false)
     const style = {color:"white", opacity:"90%"}
     return ( 
-        <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>        
+        <div className=' mx-auto flex justify-between items-center p-4 absolute top-0 z-50 w-full'>        
         {/*Left Side*/}
-            <div className='flex items-center'>
-                <div className='cursor-pointer'>
-                    <AiOutlineMenu size={30} onClick = {()=> setNav(!nav)}/>
-                </div>
-                <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'> 
-                    Top <span className='font-bold'>BBQ</span>
-                </h1 >
-                <div className='hidden lg:flex items-center'>
-                    <p className='px-2'>Home</p>
-                    <p className='px-2'> Restaraunt</p>  
-                    <p className='px-2'>Menu</p>  
-                    <p className='px-2'>Photos</p>  
+            <div className='cursor-pointer flex absolute left-4 top-4'>
+                <AiOutlineMenu className='lg:hidden'style={style} size={30} onClick = {()=> setNav(!nav)}/>
+            </div>
+            <div className='flex items-center justify-center w-full'>
+                
+                <div className='flex justify-center items-center text-xl cursor-pointer text-white/70 '>
+                    <p className='pr-8 sm-hidden '>HOME</p>
+                    <p className='pr-8 sm-hidden'> RESTARAUNT</p>  
+                    <img className='px-2 pr-6 opacity-80' src={topBBQLogo} />
+                    <p className='pr-8 sm-hidden'>MENU</p>  
+                    <p className='pr-8 sm-hidden'>PHOTOS</p>  
 
                 </div>
             </div>
@@ -31,16 +30,19 @@ const Navbar = () => {
             {/*Overlay*/}
             {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 lect-0'></div> : ''}
             {/*Side Drawer Menu*/}
-            <div className={nav ? 'fixed top-0 left-0 w-screen h-screen bg-white z-10 duration-300': 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'} >
+            <div className={nav ? 'fixed top-0 left-0 w-screen h-screen bg-white z-10 duration-700': 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'} >
                 <AiOutlineClose style={style} size = {30} className='absolute left-4 top-4 cursor-pointer' onClick = {()=> setNav(!nav)}/>
                 <div className='bg-black/95 h-screen'>
                     <nav>
+                        <div className='flex justify-center pt-6 '>
+                            <img src={topBBQLogo} />
+                        </div>
+                        
                         <ul className='flex flex-col justify-center items-center h-screen '>
-                            <h2 className='text-4xl fixed top-10 text-white'>Top <span className='font-bold'>BBQ</span></h2>
-                            <li className='text-3xl pb-8 text-white/80 '>HOME</li>
-                            <li className='text-3xl pb-8 text-white/80 flex-center'>RESTAURANT</li>
-                            <li className='text-3xl pb-8 text-white/80'>MENU</li>
-                            <li className='text-3xl pb-28 text-white/80'>PHOTOS</li>
+                            <li className='text-4xl pb-8 text-white/80 '>HOME</li>
+                            <li className='text-4xl pb-8 text-white/80 flex-center'>RESTAURANT</li>
+                            <li className='text-4xl pb-8 text-white/80'>MENU</li>
+                            <li className='text-4xl pb-28 text-white/80'>PHOTOS</li>
 
 
                         </ul>
