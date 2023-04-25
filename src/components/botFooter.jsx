f0c58b984644cd62ffc2af740d75fd732a97d333
andy phu <andyphu7601@gmail.com>
2023-04-24 21:43:43 -0700

    small note

--- src/components/botFooter.jsx
+++ src/components/botFooter.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const botFooter = () => {
     return (
-        <div className='bg-zinc-900 py-20 flex justify-center text-center text-gray-500'>
-            TOP BBQ & NOODLE &nbsp; 1425 ARTESIA BLvD UNIT 21 GARDENA, CA 90248 &nbsp; (310) 819-8413
+        <div>
+            <div className='bg-zinc-900 pt-20 flex justify-center text-center text-gray-500'>
+                TOP BBQ & NOODLE &nbsp; 1425 ARTESIA BLvD UNIT 21 GARDENA, CA 90248 &nbsp; (310) 819-8413
+            </div>
+            <div className='bg-zinc-900 pt-10 pb-20 flex justify-center text-center text-gray-500'>
+                UNOFFICIAL WEBSITE FOR TOP BBQ & NOODLE
+            </div>
         </div>
     );
 }
